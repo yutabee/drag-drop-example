b8e097a933cb3fe81392db08f957541f282632ba
yutabee <bsk87m@gmail.com>
2022-07-01 17:23:23 +0900

    map関数実装

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css';
 
 function App() {
 
-  const [items] = useState(['item0', 'item1','item2']);
+  const [items] = useState([
+    { id: 0, text: 'item0' },
+    { id: 1, text: 'item1' },
+    { id: 2, text: 'item2' },
+  ]);
 
   const onDragEnd = (result) => {
     // console.log(result);
@@ -26,39 +30,19 @@ function App() {
         <Droppable droppableId='droppable'>
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              <Draggable draggableId='item0' index={0}>
+              {items.map((item, index) => (
+              <Draggable draggableId={item.text} index={index} key={item.id}>
                 {(provided) => (
                   <div
                     className='item'
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}>
-                    {items[0]}
+                    {item.text}
                   </div>
                 )}
               </Draggable>
-               <Draggable draggableId='item1' index={1}>
-                {(provided) => (
-                  <div
-                    className='item'
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}>
-                    {items[1]}
-                  </div>
-                )}
-              </Draggable>
-               <Draggable draggableId='item2' index={2}>
-                {(provided) => (
-                  <div
-                    className='item'
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}>
-                    {items[2]}
-                  </div>
-                )}
-              </Draggable>
+              ))}
               {provided.placeholder} {/*作法として記述しておかないとコンソールにエラーが出る*/}
             </div>
           )}
