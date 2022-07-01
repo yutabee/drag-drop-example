8023538e04b0f6ab968f77787036fe9940164920
yutabee <bsk87m@gmail.com>
2022-07-01 16:19:53 +0900

    react-beautiful-dnd

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <DragDropContext> 
+        <Droppable droppableId='droppable'>
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Draggable draggableId='item0' index={0}>
+                {(provided) => (
+                  <div ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}>
+                    item0
+                  </div>
+                )}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default App;
